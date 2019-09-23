b061d3b97d46289ac28b34921e303f829dbfe3af
Mandeep Ranjit <Mandeep Ranjit@LAPTOP-SMV5472A>
2019-09-24 09:17:02 +1200

    chh

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("011f6c88-b068-458b-8caf-5525e669a414")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarRace;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRacingCar()
+        {
+            string expected = "Busted" + Environment.NewLine + "bob won 120" + Environment.NewLine + "ai won 140" + Environment.NewLine;
+            MainForm obj = new MainForm(false, true, true, 50);
+            string actual = obj.unitTest();
+            Assert.AreEqual(expected, actual);
+
+
+        }
+    }
+}
